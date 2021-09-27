6e1b53f2aa37b1dd559e297daab50673653a5a1d
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2021-09-27 09:55:07 -0500

    Se muda el body a app.js-el head a index.tml-el archivo css a App.css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,76 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+            <ul className="navBar1">
+                <li>
+                    <img className="logo" src="./media/logoVentas.png" alt="LogoVentas"/>
+                </li>
+                <ul className="navBarinterno">
+                    <li className="navBarinterno">
+                        NOMBRE DE USUARIO 
+                        <br/>
+                        ROL
+                    </li>
+                    <li>
+                        <button className="button mainButton">SALIR</button>
+                    </li>
+                </ul> 
+                
+            </ul>
+        </div>
+        <div>
+            <ul className="navBar2">
+                <li>
+                    <button className="button secundaryButton">VENTAS</button>
+                </li>
+                <li>
+                    <button className="button secundaryButton">PRODUCTOS</button>
+                </li>
+                <li>
+                    <button className="button secundaryButton">USUARIOS</button>
+                </li>
+            </ul>
+        </div>
       </header>
+      <main>
+        <ul>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+            <li>CONTENIDO</li>
+        </ul>
+      </main>
+      <footer>
+        <ul className="piePagina">
+            <li className="navBarinterno">Tienes dudas? contactanos</li>
+            <li>
+                <ul>
+                    <li>Correo</li>
+                    <li>dirreccion</li>
+                    <li>telefono</li>
+                </ul>
+            </li>
+            
+        </ul>
+      </footer>
     </div>
   );
 }
