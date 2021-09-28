ca83a77577419b183d05f3a4d2bb482217f00ec6
OskrGutierrez <osagutierrezbo@unal.edu.co>
2021-09-27 19:37:26 -0500

    Se crearon los componentes Cabeza y PiePagina bases

---
+++ src/components/PiePagina.jsx
@@ -0,0 +1,18 @@
+function PiePagina(){
+    return(
+        <footer>
+                <ul className="piePagina">
+                    <li className="navBarinterno">Tienes dudas? contactanos</li>
+                    <li>
+                        <ul>
+                            <li>Correo</li>
+                            <li>dirreccion</li>
+                            <li>telefono</li>
+                        </ul>
+                    </li>
+                </ul>
+            </footer>  
+
+    )
+}
+export default PiePagina;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,43 +1,12 @@
 import logo from './logo.svg';
 import './styles/App.css';
 import logoEmpresa from './media/logoVentas.png';
+import Cabeza from '../components/Cabeza';
 
 function Index(){
     return (
         <div>
-            <header>
-                <div>
-                    <ul className="navBar1">
-                        <li>
-                            <img className="logo" src={logoEmpresa} alt="LogoVentas"/>
-                        </li>
-                        <ul className="navBarinterno">
-                            <li className="navBarinterno">
-                                NOMBRE DE USUARIO 
-                                <br/>
-                                ROL
-                            </li>
-                            <li>
-                                <button className="button mainButton">SALIR</button>
-                            </li>
-                        </ul> 
-                
-                    </ul>
-                </div>
-                <div>
-                    <ul className="navBar2">
-                        <li>
-                            <button className="button secundaryButton">VENTAS</button>
-                        </li>
-                        <li>
-                            <button className="button secundaryButton">PRODUCTOS</button>
-                        </li>
-                        <li>
-                            <button className="button secundaryButton">USUARIOS</button>
-                        </li>
-                    </ul>
-                </div>
-            </header>
+            <Cabeza></Cabeza>
             <main>
                 <ul>
                     <li>CONTENIDO</li>
@@ -62,18 +31,7 @@ function Index(){
                     <li>CONTENIDO</li>
                 </ul>
             </main>
-            <footer>
-                <ul className="piePagina">
-                    <li className="navBarinterno">Tienes dudas? contactanos</li>
-                    <li>
-                        <ul>
-                            <li>Correo</li>
-                            <li>dirreccion</li>
-                            <li>telefono</li>
-                        </ul>
-                    </li>
-                </ul>
-            </footer>    
+              
         </div>
     )
 }
