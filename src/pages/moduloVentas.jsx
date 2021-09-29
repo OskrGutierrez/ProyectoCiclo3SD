170409c9dea4811ab533f42973a2bee4773e8367
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2021-09-29 10:01:24 -0500

    Se craa la base del formulario y la hoja de estilos para Ventas

--- src/pages/moduloVentas.jsx
+++ src/pages/moduloVentas.jsx
@@ -1,10 +1,17 @@
-import '../styles/App.css';
+import '../styles/Ventas.css';
 
 function ModuloVentas(){
     return (
-        
             <>
                 <ul>
+                    <form action="">
+                        <input type="email" required/>
+                        <input type="file" required/>
+                        <input type="number" max={100} required/>
+                        <input type="password" required />
+                        <input type="text" required />
+                        <button>Submit</button>
+                    </form>
                     <li>Este es el modulo de las ventas</li>
                     <li>Este es el modulo de las ventas</li>
                     <li>Este es el modulo de las ventas</li>
