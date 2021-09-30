ba03fcd7f040040097f6e01c0fe257c6eb3e73b3
BufferKoz <5794102@gmail.com>
2021-09-29 20:08:06 -0500

    cualquier cambio :v

--- src/components/Cabeza.jsx
+++ src/components/Cabeza.jsx
@@ -1,42 +1,43 @@
-import '../styles/App.css';
-import logoEmpresa from '../media/logoVentas.png';
-import ModulesButton from './ModulesButton';
-import { Link } from 'react-router-dom';
+import "../styles/App.css";
+import logoEmpresa from "../media/logoVentas.png";
+import ModulesButton from "./ModulesButton";
+import { Link } from "react-router-dom";
 
 function Cabeza() {
-    return (
+  return (
+    <div>
+      <header>
         <div>
-            <header>
-                <div>
-                    <ul className="navBar1">
-                        <li>
-                            <img className="logo" src={logoEmpresa} alt="LogoVentas" />
-                        </li>
-                        <ul className="navBarinterno">
-                            <li className="navBarinterno">
-                                NOMBRE DE USUARIO
-                                <br />
-                                ROL
-                            </li>
-                            <li>
-                                <Link to='Inicio'>
-                                <button className="button mainButton">SALIR</button>
-                                </Link>
-                            </li>
-                        </ul>
-
-                    </ul>
-                </div>
-                <div>
-                    <ul className="navBar2">
-                        <ModulesButton buttonName='VENTAS' modulePage='moduloVentas' />
-                        <ModulesButton buttonName='PRODUCTOS' modulePage='moduloUsuarios' />
-                        <ModulesButton buttonName='USUARIOS' modulePage='moduloProductos' />
-                    </ul>
-                </div>
-            </header>
+          <ul className="navBar1">
+            <li>
+              <img className="logo" src={logoEmpresa} alt="LogoVentas" />
+            </li>
+            <ul className="navBarinterno">
+              <li className="navBarinterno">
+                NOMBRE DE USUARIO
+                <br />
+                ROL
+              </li>
+              <li>
+                <Link to="Inicio">
+                  <button className="button mainButton">SALIR</button>
+                </Link>
+              </li>
+            </ul>
+          </ul>
         </div>
-
-    )
+        <div>
+          <ul className="navBar2">
+            <ModulesButton buttonName="VENTAS" modulePage="moduloVentas" />
+            <ModulesButton
+              buttonName="PRODUCTOS"
+              modulePage="moduloProductos"
+            />
+            <ModulesButton buttonName="USUARIOS" modulePage="moduloUsuarios" />
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
 }
 export default Cabeza;
