5c5e93ff067addd27c68c77475f11858bd942286
Daniela Quiceno <danielaqg1@gmail.com>
2021-09-30 19:23:34 -0500

    Añadir pagina modificación venta

---
+++ src/pages/modifVenta.jsx
@@ -0,0 +1,22 @@
+import '../styles/App.css';
+
+function ModifVenta(){
+    return (
+        
+            <>
+                <section>
+                    <ul>
+                    <li>Aqui estan registradas las ventas</li>
+                    <li>Aqui estan registradas las ventas</li>
+                    <li>Aqui estan registradas las ventas</li>
+                    <li>Aqui estan registradas las ventas</li>
+                    <li>Aqui estan registradas las ventas</li>  
+                    </ul>
+                    </section>
+                  
+                
+            </>
+            
+    )
+}
+export default ModifVenta;
