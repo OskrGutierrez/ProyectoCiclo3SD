8e012a6e859426c6080373f4523c71f0e30663b8
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2021-10-01 18:33:46 -0500

    SE CREA LA TABLA PARA MOSTRAR LOS DATOS DE VENTAS

--- src/pages/nuevaVenta.jsx
+++ src/pages/nuevaVenta.jsx
@@ -21,6 +21,52 @@ function NuevaVenta(){
                             <button className="botones" type="submit">Agregar Producto</button>
                         </form>
                     </div>
+                    <div className="tablaNuevaVenta">
+                        <table>
+                            <tr>
+                                <th>ITEM</th>
+                                <th>PRODUTO</th>
+                                <th>CANTIDAD</th>
+                                <th>PRECIO UNIDAD</th>
+                                <th>SUB-TOTAL</th>
+                            </tr>
+                            <tr>
+                                <th>001</th>
+                                <th>LECHE</th>
+                                <th>2</th>
+                                <th>2800</th>
+                                <th>5600</th>
+                            </tr>
+                            <tr>
+                                <th>002</th>
+                                <th>HUEVOS</th>
+                                <th>12</th>
+                                <th>300</th>
+                                <th>3600</th>
+                            </tr>
+                            <tr>
+                                <th>003</th>
+                                <th>AZUCAR</th>
+                                <th>1</th>
+                                <th>3000</th>
+                                <th>3000</th>
+                            </tr>
+                            <tr>
+                                <th>004</th>
+                                <th>MANZANA</th>
+                                <th>2</th>
+                                <th>1000</th>
+                                <th>2000</th>
+                            </tr>
+                            <tr>
+                                <th>005</th>
+                                <th>NARANJA</th>
+                                <th>4</th>
+                                <th>800</th>
+                                <th>3200</th>
+                            </tr>
+                        </table>
+                    </div>
                 </ul>
                 <br />
                 <br />
